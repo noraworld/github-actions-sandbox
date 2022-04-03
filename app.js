be28063d3975983493aa6f938387ba12b90bf6ea
noraworld <mail@noraworld.com>
2022-04-03 14:14:45 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    core.debug('Hello from core.debug');
+    console.log('Hello from console.log');
+    core.debug(github);
+    console.log(github);
+  }
+  catch(error) {
+    core.setFailed(error.message);
+  }
+}
