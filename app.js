8991861e1828befe7dc598bcb8bfb4b4f108a9c9
Kosuke Aoki <mail@noraworld.com>
2023-06-30 15:14:20 +0900

    Restore

--- app.js
+++ app.js
@@ -5,23 +5,23 @@ async function run() {
 
   const repo = process.env.GITHUB_REPOSITORY;
   const issueNumber = process.env.ISSUE_NUMBER;
-  console.log('can you see me?')
-  console.log(repo)
-  console.log(issueNumber)
+  // console.log('can you see me?')
+  // console.log(repo)
+  // console.log(issueNumber)
   // const issueNumber = process.env.GITHUB_EVENT.issue.number;
 
-  // const { data: comments } = await octokit.issues.listComments({
-  //   owner,
-  //   repo,
-  //   issue_number: issueNumber,
-  // });
+  const { data: comments } = await octokit.issues.listComments({
+    owner,
+    repo,
+    issue_number: issueNumber,
+  });
 
-  // comments.forEach((comment) => {
-  //   console.log(`Comment ID: ${comment.id}`);
-  //   console.log(`Author: ${comment.user.login}`);
-  //   console.log(`Body: ${comment.body}`);
-  //   console.log('---');
-  // });
+  comments.forEach((comment) => {
+    console.log(`Comment ID: ${comment.id}`);
+    console.log(`Author: ${comment.user.login}`);
+    console.log(`Body: ${comment.body}`);
+    console.log('---');
+  });
 }
 
 run().catch((error) => {
