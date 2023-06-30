2a57b5ac26b97aae5ca793b4b3da40c3d6c68766
Kosuke Aoki <mail@noraworld.com>
2023-06-30 18:20:43 +0900

    app.js

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-const { Octokit } = require('@octokit/rest');
-const fs = require('fs');
-const { execSync } = require('child_process');
+'use strict'
+
+const { Octokit } = require('@octokit/rest')
+const fs = require('fs')
+const { execSync } = require('child_process')
 const path = require('path')
 
 async function run() {
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
 
-  const repository = process.env.GITHUB_REPOSITORY;
+  const repository = process.env.GITHUB_REPOSITORY
   const [ owner, repo ] = repository.split('/')
-  const issueNumber = process.env.ISSUE_NUMBER;
-  // console.log('can you see me?')
-  // console.log(repo)
-  // console.log(issueNumber)
-  // const issueNumber = process.env.GITHUB_EVENT.issue.number;
-
-  // const { data: comments } = await octokit.issues.listComments({
-  //   owner,
-  //   repo,
-  //   issue_number: issueNumber,
-  // });
+  const issueNumber = process.env.ISSUE_NUMBER
 
   let comments = []
   let page = 1
@@ -40,11 +32,9 @@ async function run() {
 
   let content = ''
   comments.forEach((comment) => {
-    // console.log(`comment: ${JSON.stringify(comment)}`);
     content += `${comment.body}\n\n---\n\n`
-  });
+  })
 
-  // console.log(`filepath: ${process.env.FILEPATH}`)
   const filename = process.env.FILEPATH
   const dir = path.dirname(filename)
 
@@ -54,8 +44,7 @@ async function run() {
     fs.mkdirSync(dir, { recursive: true })
   }
 
-  console.log(`process.env.ISSUE_BODY: ${process.env.ISSUE_BODY}`)
-  fs.writeFileSync(filename, `${existingContent}${process.env.ISSUE_BODY}\n\n---\n\n${content}`);
+  fs.writeFileSync(filename, `${existingContent}${process.env.ISSUE_BODY}\n\n---\n\n${content}`)
 
   execSync('git config --global user.email "mail@noraworld.com"')
   execSync('git config --global user.name "Kosuke Aoki"')
@@ -65,6 +54,6 @@ async function run() {
 }
 
 run().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+  console.error(error)
+  process.exit(1)
+})
