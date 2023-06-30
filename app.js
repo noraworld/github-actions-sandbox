97b7aaf83107da23867630502b2774d54af6fcec
Kosuke Aoki <mail@noraworld.com>
2023-06-30 14:33:19 +0900

    Add ability to retrieve comments from issues

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const { Octokit } = require('@octokit/rest');
 
 async function run() {
-  try {
-    core.debug('Hello from core.debug');
-    console.log('Hello from console.log');
-    core.debug(github);
-    console.log(github);
-  }
-  catch(error) {
-    core.setFailed(error.message);
-  }
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  const { owner, repo } = process.env.GITHUB_REPOSITORY;
+  const issueNumber = process.env.GITHUB_EVENT.issue.number;
+
+  const { data: comments } = await octokit.issues.listComments({
+    owner,
+    repo,
+    issue_number: issueNumber,
+  });
+
+  comments.forEach((comment) => {
+    console.log(`Comment ID: ${comment.id}`);
+    console.log(`Author: ${comment.user.login}`);
+    console.log(`Body: ${comment.body}`);
+    console.log('---');
+  });
 }
 
-run();
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
