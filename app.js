b2bafa461f35da0af256dd04752f22e980b2e355
Kosuke Aoki <mail@noraworld.com>
2023-06-30 14:45:29 +0900

    debug: githubRef

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ async function run() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
   const { owner, repo } = process.env.GITHUB_REPOSITORY;
-  const issueNumber = process.env.GITHUB_EVENT.issue.number;
+  const githubRef = process.env.GITHUB_REF;
+  console.log(githubRef)
+  // const issueNumber = process.env.GITHUB_EVENT.issue.number;
 
-  const { data: comments } = await octokit.issues.listComments({
-    owner,
-    repo,
-    issue_number: issueNumber,
-  });
+  // const { data: comments } = await octokit.issues.listComments({
+  //   owner,
+  //   repo,
+  //   issue_number: issueNumber,
+  // });
 
-  comments.forEach((comment) => {
-    console.log(`Comment ID: ${comment.id}`);
-    console.log(`Author: ${comment.user.login}`);
-    console.log(`Body: ${comment.body}`);
-    console.log('---');
-  });
+  // comments.forEach((comment) => {
+  //   console.log(`Comment ID: ${comment.id}`);
+  //   console.log(`Author: ${comment.user.login}`);
+  //   console.log(`Body: ${comment.body}`);
+  //   console.log('---');
+  // });
 }
 
 run().catch((error) => {
