4755051862f5619874c14f4441d41a36cab4ffa4
Kosuke Aoki <mail@noraworld.com>
2023-06-30 14:52:31 +0900

    debug

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const { Octokit } = require('@octokit/rest');
 async function run() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-  const { owner, repo } = process.env.GITHUB_REPOSITORY;
-  const githubRef = process.env.GITHUB_REF;
+  const repo = process.env.GITHUB_REPOSITORY;
+  const issueNumber = process.env.ISSUE_NUMBER;
   console.log('can you see me?')
-  console.log(owner)
   console.log(repo)
-  console.log(githubRef)
+  console.log(issueNumber)
   // const issueNumber = process.env.GITHUB_EVENT.issue.number;
 
   // const { data: comments } = await octokit.issues.listComments({
