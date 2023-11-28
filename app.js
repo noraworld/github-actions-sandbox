d4e03e1bccea0dc1b9a3cd79a4f04b0cdccca41e
Kosuke Aoki <mail@noraworld.com>
2023-11-29 01:34:54 +0900

    async, await

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ const { DateTime } = require('luxon')
 const newline = '\r\n'
 
 async function run() {
+  const issueBody = process.env.ISSUE_BODY ? `${process.env.ISSUE_BODY}${newline}` : ''
+  let comments = getComments()
+  let content = buildContent(comments, issueBody)
+  let modes = process.env.MODE.split(',').map((element) => element.trim())
+
+  for (const mode of modes) {
+    switch (mode) {
+      case 'file':
+        commit(issueBody, content)
+        break
+      case 'issue':
+        post(issueBody, content)
+        break
+      default:
+        console.error(`unknown mode: ${process.env.MODE}`)
+        process.exit(1)
+        break
+    }
+  }
+}
+
+async function getComments() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
 
   const repository = process.env.GITHUB_REPOSITORY
@@ -34,9 +56,33 @@ async function run() {
     page++
   } while (response.data.length === perPage)
 
+  return comments
+}
+
+function buildContent(comments, issueBody) {
+  let content = ''
+  let isFirstComment = true
+
+  comments.forEach((comment) => {
+    if (!isFirstComment || issueBody) {
+      content += `${newline}---${newline}${newline}`
+    }
+    isFirstComment = false
+
+    content += comment.body
+
+    if (process.env.WITH_DATE) {
+      content += `${newline}${newline}> ${formattedDateTime(comment.created_at)}`
+    }
+
+    content += `${newline}`
+  })
+
+  return content
+}
+
+function commit(issueBody, content) {
   const filepath = process.env.FILEPATH
-  const dir = path.dirname(filepath)
-  const issueBody = process.env.ISSUE_BODY ? `${process.env.ISSUE_BODY}${newline}` : ''
 
   let existingContent = ''
   let commitMessage = ''
@@ -53,23 +99,7 @@ async function run() {
     header = `${process.env.WITH_HEADER}${newline}${newline}`
   }
 
-  let content = ''
-  let isFirstComment = true
-  comments.forEach((comment) => {
-    if (!isFirstComment || issueBody) {
-      content += `${newline}---${newline}${newline}`
-    }
-    isFirstComment = false
-
-    content += comment.body
-
-    if (process.env.WITH_DATE) {
-      content += `${newline}${newline}> ${formattedDateTime(comment.created_at)}`
-    }
-
-    content += `${newline}`
-  })
-
+  const dir = path.dirname(filepath)
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true })
   }
@@ -81,6 +111,24 @@ async function run() {
   execSync(`git add "${filepath}"`)
   execSync(`git commit -m "${commitMessage}"`)
   execSync('git push')
+}
+
+function post(issueBody, content) {
+  targetIssueRepo = process.env.TARGET_ISSUE_REPO ? process.env.ISSUE_REPO : process.env.GITHUB_REPOSITORY
+
+  if (process.env.TARGET_ISSUE_NUMBER && process.env.TARGET_ISSUE_NUMBER !== 'latest') {
+    targetIssueNumber = process.env.TARGET_ISSUE_NUMBER
+  }
+  else {
+    targetIssueNumber = execSync(`gh issue list --repo "${targetIssueRepo}" --limit 1 | awk '{ print $1 }'`)
+  }
+
+  let header = ''
+  if (process.env.WITH_HEADER) header = `${process.env.WITH_HEADER}${newline}${newline}`
+
+  let title = `# ✅ ${process.env.ISSUE_TITLE}${newline}`
+
+  execSync(`gh issue comment --repo "${targetIssueRepo}" "${targetIssueNumber}" --body "${header}${title}${issueBody}${content}"`)
 }
 
 function formattedDateTime(timestamp) {
