be4f2d91340245bbb411037065ef51d1cef5efee
Kosuke Aoki <mail@noraworld.com>
2025-05-04 18:23:17 +0900

    use GITHUB_EVENT_PATH

--- dispatch-inputs-log-test.js
+++ dispatch-inputs-log-test.js
@@ -1,5 +1,9 @@
-console.log(`process.argv[2]: ${process.argv[2]}`);
-console.log(`process.env.ENV_VAR: ${process.env.ENV_VAR}`);
-console.log(process.env);
+const fs = require('fs');
+const json = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
+console.log(`json.inputs.foo = ${json.inputs.foo}`);
+
+// console.log(`process.argv[2]: ${process.argv[2]}`);
+// console.log(`process.env.ENV_VAR: ${process.env.ENV_VAR}`);
+// console.log(process.env);
 // console.log(`inputs.foo: ${process.env.inputs.foo}`);
 // console.log(`github.event.inputs.foo: ${process.env.github.event.inputs.foo}`);
