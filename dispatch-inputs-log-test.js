44c64a3ec4cf9bad0dd6d5e3789c710dc1daa731
Kosuke Aoki <mail@noraworld.com>
2025-05-04 18:04:51 +0900

    delete argv & env

--- dispatch-inputs-log-test.js
+++ dispatch-inputs-log-test.js
@@ -1,5 +1,5 @@
 console.log(`process.argv[2]: ${process.argv[2]}`);
 console.log(`process.env.ENV_VAR: ${process.env.ENV_VAR}`);
-console.log(`process.env: ${process.env}`);
-console.log(`inputs.foo: ${process.env.inputs.foo}`);
-console.log(`github.event.inputs.foo: ${process.env.github.event.inputs.foo}`);
+console.log(process.env);
+// console.log(`inputs.foo: ${process.env.inputs.foo}`);
+// console.log(`github.event.inputs.foo: ${process.env.github.event.inputs.foo}`);
