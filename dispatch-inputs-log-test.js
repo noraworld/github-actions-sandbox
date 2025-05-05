2f300b10f9f53c160d7be7e9dfc78356595b1087
Kosuke Aoki <mail@noraworld.com>
2025-05-05 13:14:46 +0900

    10 個以上の inputs

--- dispatch-inputs-log-test.js
+++ dispatch-inputs-log-test.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 const json = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH));
-console.log(`json.inputs.foo = ${json.inputs.foo}`);
 
-// console.log(`process.argv[2]: ${process.argv[2]}`);
-// console.log(`process.env.ENV_VAR: ${process.env.ENV_VAR}`);
-// console.log(process.env);
-// console.log(`inputs.foo: ${process.env.inputs.foo}`);
-// console.log(`github.event.inputs.foo: ${process.env.github.event.inputs.foo}`);
+console.log(`json.inputs.foo    = ${json.inputs.foo}`);
+console.log(`json.inputs.bar    = ${json.inputs.bar}`);
+console.log(`json.inputs.baz    = ${json.inputs.baz}`);
+console.log(`json.inputs.qux    = ${json.inputs.qux}`);
+console.log(`json.inputs.quux   = ${json.inputs.quux}`);
+console.log(`json.inputs.corge  = ${json.inputs.corge}`);
+console.log(`json.inputs.grault = ${json.inputs.grault}`);
+console.log(`json.inputs.garply = ${json.inputs.garply}`);
+console.log(`json.inputs.waldo  = ${json.inputs.waldo}`);
+console.log(`json.inputs.fred   = ${json.inputs.fred}`);
+console.log(`json.inputs.plugh  = ${json.inputs.plugh}`);
+console.log(`json.inputs.xyzzy  = ${json.inputs.xyzzy}`);
+console.log(`json.inputs.thud   = ${json.inputs.thud}`);
